81057b64abe689bfc333a778a4168af411f46e15
Ksu1106 <skywolfmarkova11@gmail.com>
2024-11-19 17:36:48 +0300

    Создано функция загрузки изображения в метку

--- dogs_1.py
+++ dogs_1.py
@@ -1,7 +1,37 @@
+from logging import exception
 from tkinter import *
+from tkinter import messagebox
 import requests
 from PIL import Image, ImageTk
 from io import BytesIO
+
+from pyexpat.errors import messages
+
+def get_dog_image():
+
+
+
+
+
+
+
+
+def show_image():
+    image_url = get_dog_image()
+    if image_url:
+        try:
+            response = requests.get(image_url, stream = True)
+            response.raise_for_status()
+            img_data = BytesIO(response.content)
+            img = Image.open(img_data)
+            img.thumbnail((300,300))
+            label.config(image=img)
+            label.image = img
+        except Exception as e:
+            messagebox.showerror('Ошибка',f'Возникла ошибка: {e}')
+
+
+
 
 window = Tk()
 window.title('Картинка с собачками')
